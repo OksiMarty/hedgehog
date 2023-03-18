57b29f0779810b55ee54eda22e3c3c74da42411e
Oksy Marty <hrechkasweets@gmail.com>
2023-03-18 15:28:12 +0200

    Add privat api

--- src/app.js
+++ src/app.js
@@ -16,9 +16,21 @@ function newTime() {
   let time = document.getElementById("time");
 
   let date = new Date();
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
+  let hours = date.getHours().toString().padStart(2, "0");
+  let minutes = date.getMinutes().toString().padStart(2, "0");
   time.textContent = `${hours}:${minutes}`;
 }
 
 setInterval(newTime, 1000);
+
+function getRate() {
+  const api =
+    "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11";
+  return fetch(api)
+    .then((response) => response.json())
+    .then(function (data) {
+      let usdBuyRate = document.getElementById("buyUsd");
+      usdBuyRate.innerText = data[1].buy;
+    });
+}
+getRate();
